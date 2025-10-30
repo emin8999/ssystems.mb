72045dcbb53cc944449c708a73e92173450bede5
seid1zahidov <seid.zahidov15@gmail.com>
2025-10-30 15:48:53 +0400

    adds contact page and back end folder sturktur

---
+++ Front/src/components/Header/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+
+const Index = () => {
+  return (
+    <div className="w-full bg-custom-green flex flex-col justify-center text-white font-inter p-4 md:p-16">
+      <div className="md:ml-20">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-semibold mb-4">
+          Get Connect with Us
+        </h1>
+        <p className="w-full sm:w-3/4 md:w-2/5 font-light text-base leading-6 tracking-normal">
+          SSYSTEMS MB serves customers in Lithuania and nearby areas, providing
+          fast and reliable repair solutions for homes and businesses. Whether
+          you’re in the city center or outskirts, we’re here to help.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
